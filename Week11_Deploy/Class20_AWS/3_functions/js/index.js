b62ed54f79b3020453fca25620a04515cb0d4120
Jonathan Boswell <jonathan@arborbridge.com>
2017-08-01 21:48:52 -0700

    Class 20 updates

--- Week11_Deploy/Class20_AWS/3_functions/js/index.js
+++ Week11_Deploy/Class20_AWS/3_functions/js/index.js
@@ -6,12 +6,20 @@ $(function(){
         sayHello()
 
         //can you pass sayHello a name and have it output "Hello " + name?
+        sayHello("Josh");
 
         //can you call a function sayGoodbye()?
+        sayGoodbye();
 
         //can you create a function say() that takes TWO parameters: name and greeting
         // where you can pass a name and a greeting and it will say like "Hello Jonathan"
+        say("Hello", "Jonathan");
     });
+
+    function say(greeting, name)
+    {
+        $('#output').append(greeting + " " + name + '\n');
+    }
 
     //This is a function definition or declaration
     //It begins with the word "function"
@@ -20,7 +28,7 @@ $(function(){
     {
         if(name)
         {
-            //does nothing yet :)
+            $('#output').append('Hello ' + name + '\n');
         }
     }
 
